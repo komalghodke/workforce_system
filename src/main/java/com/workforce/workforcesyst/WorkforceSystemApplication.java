0c1bce111420edc31fe1dd36f4264f893511b1da
komalghodke <komalghodke99@gmail.com>
2024-08-31 20:52:34 +0530

    Initial commit

---
+++ src/main/java/com/workforce/workforcesyst/WorkforceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.workforce.workforcesyst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkforceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkforceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workforce/workforcesyst/WorkforceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workforce.workforcesyst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkforceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
