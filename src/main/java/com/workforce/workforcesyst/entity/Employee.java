7975ae63a884caed8ae83532c5acb7a03286cc7f
komalghodke <komalghodke99@gmail.com>
2024-09-01 20:25:27 +0530

    Added new entity

---
+++ src/main/java/com/workforce/workforcesyst/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.workforce.workforcesyst.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name ="Employee")
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	//@Column(name="first_name")
+	private String firstName;
+	private String lastName;
+	private String emailId;
+}
